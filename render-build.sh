c725b120f33b2f6e53c863392e8029f139037314
Nicolas Taborda Gomez <nicolas.taborda@correounivalle.edu.co>
2024-12-06 12:11:54 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Configurar JAVA_HOME
 export JAVA_HOME=/opt/render/java-17
 export PATH=$JAVA_HOME/bin:$PATH
+
+# Ejecutar el comando de construcción con Maven Wrapper
 ./mvnw clean package
